c5537ee39c1e37dec2a2d411b1cf922ed8ac8f8d
ValeKir <alexkir15@yandex.ru>
2025-03-09 01:35:37 +0300

    Пофиксил ошибки

--- src/pages/feed/lib/handleScroll.js
+++ src/pages/feed/lib/handleScroll.js
@@ -1,13 +1,13 @@
-import {loadImages} from './loadImages';
-import {debounce} from '../../../shared/utils/debounce';
-
-const scrollHandler = async () => {
-    const threshold = 500;
-    const scrolledToBottom = window.innerHeight + window.scrollY >= document.body.offsetHeight - threshold;
-
-    if (scrolledToBottom) {
-        await loadImages();
-    }
-};
-
-export const debouncedScroll = debounce(scrollHandler(), 500);
+// import {loadImages} from './loadImages';
+// import {debounce} from '../../../shared/utils/debounce';
+//
+// export const scrollHandler = async (numPage) => {
+//     const threshold = 500;
+//     const scrolledToBottom = window.innerHeight + window.scrollY >= document.body.offsetHeight - threshold;
+//
+//     if (scrolledToBottom) {
+//         return await loadImages(numPage);
+//     }
+// };
+//
+// export const debouncedScroll = debounce(scrollHandler, 500);
--- src/pages/feed/lib/loadImages.js
+++ src/pages/feed/lib/loadImages.js
@@ -1,18 +1,14 @@
-import {API} from '../../../shared/api/api';
-import {createSkeleton} from './skeleton/skeleton';
-
-let isLoading;
-
-export const loadImages = async () => {
-    if (isLoading) return;
-    isLoading = true;
-
-    const response = await API.get('/api/v1/feed');
-    const images = JSON.parse(await response.text());
-
-    const feed = document.querySelector('#feed');
-    images.data.forEach((item) => {
-        feed.appendChild(createSkeleton(item.image));
-    });
-    isLoading = false;
-};
+// import {API} from '../../../shared/api/api';
+//
+// let isLoading;
+//
+// export const loadImages = async (numPage) => {
+//     if (isLoading) return;
+//     isLoading = true;
+//
+//     const response = await API.get(`/api/v1/feed?page${numPage}`);
+//     const images = JSON.parse(await response.text());
+//
+//     isLoading = false;
+//     return images;
+// };
