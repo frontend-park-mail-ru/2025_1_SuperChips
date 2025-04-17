e59ca0f0848eeabc12c101b9504f4c77ee98c554
ValeKir <alexkir15@yandex.ru>
2025-04-18 01:31:30 +0300

    небольшие изменения

--- src/widgets/sidebar/handlers/logout.ts
+++ src/widgets/sidebar/handlers/logout.ts
@@ -1,5 +1,4 @@
 import { navigate } from 'shared/router';
-import { handleClickOutside } from 'shared/handlers/handleClickOutside';
 import { Auth } from 'features/authorization';
 
 export const logoutHandler = async (event: Event) => {
@@ -9,18 +8,17 @@ export const logoutHandler = async (event: Event) => {
     const button = document.querySelector<HTMLElement>('#logout');
     if (!button) return;
 
+    const clickHandler = (e: Event) => {
+        if (!button.contains(e.target as Node)) {
+            button.classList.replace('sidebar-button_confirm', 'sidebar-button');
+            document.querySelector('#logout-toast')?.remove();
+            window.removeEventListener('click', clickHandler);
+        }
+    };
+
     if (button.classList.contains('sidebar-button')) {
         button.classList.replace('sidebar-button', 'sidebar-button_confirm');
-        window.addEventListener(
-            'click',
-            handleClickOutside(
-                button,
-                'sidebar-button_confirm',
-                'sidebar-button',
-                'logout-toast'
-            ),
-            { once: true }
-        );
+        window.addEventListener('click', clickHandler);
         const toast = '<div id="logout-toast" class="logout-toast">Вы уверены, что хотите выйти?</div>';
         document.body.insertAdjacentHTML('beforeend', toast);
     } else {
