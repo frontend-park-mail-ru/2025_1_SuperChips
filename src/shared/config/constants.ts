dba53200f01a9529c93293172e0cf8a2581ddb79
Демирел Эмре <68134981+emresha@users.noreply.github.com>
2025-04-19 13:26:08 +0300

    Update constants.ts

--- src/shared/config/constants.ts
+++ src/shared/config/constants.ts
@@ -1,5 +1,5 @@
 export const API_BASE_URL = '';
-export const MAX_IMAGE_SIZE = 3 * 1024 * 1024; // 3 МБ
+export const MAX_IMAGE_SIZE = 10 * 1024 * 1024; // 3 МБ
 export const VALID_IMAGE_EXTENSIONS = ['jpg', 'png', 'jpeg', 'gif'];
 export const USER_SAVED_PINS_BOARD = 'Сохраненные';
 export const USER_OWN_PINS_BOARD = 'Созданные вами';
