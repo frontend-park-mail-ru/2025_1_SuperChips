3f08781d136ded6ceddaf5a51e5212300bcc6752
ValeKir <alexkir15@yandex.ru>
2025-05-31 02:20:28 +0300

    поправил превью доски для соавтора

--- src/entities/Board/ui/Board.ts
+++ src/entities/Board/ui/Board.ts
@@ -2,6 +2,7 @@ import type { IBoardProps } from '../model/types';
 import { pluralize } from 'shared/utils';
 import { BoardPopup } from 'widgets/BoardPopup';
 import { appState, navigate } from 'shared/router';
+import { Auth } from 'features/authorization';
 import BoardTemplate from './Board.hbs';
 import './Board.scss';
 
@@ -12,11 +13,13 @@ import './Board.scss';
 export const Board = (params: IBoardProps) => {
     const container = document.createElement('div');
 
+    const isAuthor = Auth.userData?.id === params?.author_id;
+
     const config = {
         ...params,
         preview: ['', '', ''],
         is_private: params.is_private && params.own,
-        mutable: params.own && !params.permanent,
+        mutable: params.own && !params.permanent && isAuthor,
         flowCountPluralized: '',
         mobile: appState.mobile,
     };
