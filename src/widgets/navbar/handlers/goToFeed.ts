ad38a22268ec659f690cef8d944fbd0d2b93526e
ValeKir <alexkir15@yandex.ru>
2025-05-31 03:17:49 +0300

    smol fix

--- src/widgets/navbar/handlers/goToFeed.ts
+++ src/widgets/navbar/handlers/goToFeed.ts
@@ -4,11 +4,18 @@ import { feedState } from 'pages/FeedPage';
 
 export const goToFeed = async (event: Event) => {
     event.preventDefault();
+
     if (appState.chat.open) {
         closeChatList();
     }
-    if (appState.activePage === 'feed' && feedState.pageNum !== 3 || appState.search.isFiltered) { // изначально в ленте загружается 3 страницы пинов
+
+    if (appState.activePage === 'feed' && feedState.pageNum > 3 || appState.search.isFiltered) { // изначально в ленте загружается 3 страницы пинов
         Object.assign(feedState, { pageNum: 1, filter: 'flows', loadedPins: [], lastScroll: 0 });
+        Object.assign(appState.search, {
+            filter: 'flows', query: '', page: 1, lastQuery: '',
+            loadedPins: [], loadedBoards: [], loadedUsers: [],
+            notFound: false, isFiltered: false,
+        });
         appState.forceNavigate = true;
     }
 
